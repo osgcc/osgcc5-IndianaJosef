194b877c83e18fbc64d2400ab00dfc3f45327cc8
wilkie <wilkie05@gmail.com>
2011-11-06 01:17:44 -0400

    Represent player as a tile... for now.
    Make test world a little bigger.
    
    Clean up main.

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
 require "world"
 
 function love.draw()
-	love.graphics.print("Hello World", 400, 300)
 	love.graphics.setBackgroundColor(255,0,0)
-	love.graphics.rectangle("fill", 0, 0, 32, 32)
 
 	local world = World:new()
 	world:load("world_1")
