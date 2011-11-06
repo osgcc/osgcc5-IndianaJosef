039e2adf46bdb119c67defb63db1449c95f82fb6
wilkie <wilkie05@gmail.com>
2011-11-06 01:44:12 -0400

    Movement left and right. No collisions.

--- main.lua
+++ main.lua
@@ -1,20 +1,62 @@
+-- Require List
 require "loader"
 require "world"
 require "player"
 require "viewport"
 
+-- Load Initial World
+
+State = {REST = 0, MOVE_LEFT = 1, MOVE_RIGHT = 2}
+state = State.REST
+keys_down = 0
+
+load_data = Loader:load("world_1")
+
+world = World:new():with(load_data.map)
+player = Player:new():with(load_data.start)
+
+viewport = Viewport:new()
+
+viewport.x = 18
+viewport.y = 15
+
+load_data = nil
+
+-- Events
+
+function love.keypressed(key, unicode)
+  if key == "q" then
+    love.event.push("q")
+  elseif key == "left" then
+    state = State.MOVE_LEFT
+    keys_down = keys_down + 1
+  elseif key == "right" then
+    state = State.MOVE_RIGHT
+    keys_down = keys_down + 1
+  end
+end
+
+function love.keyreleased(key)
+  if key == "left" then
+    keys_down = keys_down - 1
+  elseif key == "right" then
+    keys_down = keys_down - 1
+  end
+  if keys_down == 0 then
+    state = State.REST
+  end
+end
+
+function love.update(dt)
+  if state == State.MOVE_LEFT then
+    player.x = player.x - (250*dt)
+  elseif state == State.MOVE_RIGHT then
+    player.x = player.x + (250*dt)
+  end
+end
+
 function love.draw()
 	love.graphics.setBackgroundColor(255,0,0)
-
-  local load_data = Loader:load("world_1")
-
-	local world = World:new():with(load_data.map)
-  local player = Player:new():with(load_data.start)
-
-  local viewport = Viewport:new()
-
-  viewport.x = 18
-  viewport.y = 15
 
   viewport:draw(world)
   viewport:draw(player)
